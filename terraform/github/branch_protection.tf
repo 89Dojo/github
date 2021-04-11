152a8ecf25e46c15145eace5f13aceb9f0d2dacf
uu-ootsubo <uu-ootsubo@kddi.com>
2021-04-11 23:10:48 +0900

    CI

--- terraform/github/branch_protection.tf
+++ terraform/github/branch_protection.tf
@@ -1,10 +1,10 @@
-resource "github_branch_protection" "github_master" {
-  enforce_admins = false
-  required_status_checks {}
-
-  required_pull_request_reviews {
-    dismiss_stale_reviews = false
-  }
-  pattern = "main"
-  repository_id = github_repository.github.id
-}
+//resource "github_branch_protection" "github_master" {
+//  enforce_admins = false
+//  required_status_checks {}
+//
+//  required_pull_request_reviews {
+//    dismiss_stale_reviews = false
+//  }
+//  pattern = "main"
+//  repository_id = github_repository.github.id
+//}
