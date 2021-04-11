e919a38a3e9fdef0721df9c095b899fe25727a92
tubone24 <tubo.yyyuuu@gmail.com>
2021-04-12 00:50:06 +0900

    WIP

--- terraform/github/repository.tf
+++ terraform/github/repository.tf
@@ -6,3 +6,15 @@ resource "github_repository" "github" {
   has_wiki      = true
   has_downloads = true
 }
+
+resource "github_issue_label" "github_bug" {
+  repository = "github"
+  name       = "Bug"
+  color      = "FF0000"
+}
+
+resource "github_issue_label" "github_feature" {
+  repository = "github"
+  name       = "Feature"
+  color      = "00FF00"
+}
