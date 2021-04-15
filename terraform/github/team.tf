211e3d343df7f98200927454aa3ae1f45df44f9f
tubone <uu-ootsubo@kddi.com>
2021-04-15 11:37:20 +0900

    Update team.tf

--- terraform/github/team.tf
+++ terraform/github/team.tf
@@ -1,9 +1,17 @@
 resource "github_team" "admin" {
   name    = "Admin"
+  description = "Admin"
   privacy = "closed"
 }
 
 resource "github_team" "developer" {
   name    = "Developer"
+  description = "developer"
   privacy = "closed"
+}
+
+resource "github_team" "readonly"
+  name        = "Readoly"
+  description = "readonly"
+  privacy     = "closed"
 }
